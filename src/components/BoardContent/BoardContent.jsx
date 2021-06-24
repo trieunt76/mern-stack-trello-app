be12c679e48a7aef9f89d93a3a2bf0c2e993d6b5
trieunt-dev <trieunt76@gmail.com>
2021-06-24 08:48:40 +0700

    [3.0.0] - Initial Sample Data

--- src/components/BoardContent/BoardContent.jsx
+++ src/components/BoardContent/BoardContent.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { isEmpty } from 'lodash';
 import './BoardContent.scss';
 import Column from 'components/Column';
+import initialData from 'mocks';
+import { mapOrder } from 'utilities';
+
 const BoardContent = () => {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+
+    useEffect(() => {
+        const data = initialData.boards.find((board) => board.id === 'board-1');
+        if (data) {
+            setBoard(data);
+
+            // Sort
+
+            setColumns(mapOrder(data.columns, data.columnOrder, 'id'));
+        }
+    }, []);
+
+    if (isEmpty(board)) {
+        return <div>No Board</div>;
+    }
+
     return (
         <div className="main-content">
-            <Column />
+            {columns.map((item, index) => {
+                return <Column key={index} {...item} />;
+            })}
         </div>
     );
 };
--- src/components/Task/Task.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Task.scss';
-const Task = () => {
-    return <li className="board__list-item">Item 1</li>;
-};
-
-export default Task;
